129be35a0c05a0ba11f4356a63aedb9f93e78508
Aiden gillespie <aiden@cold.org>
2021-08-26 14:17:43 -0600

    Updated Css in html, added notes for future links in js file

--- server/index.js
+++ server/index.js
@@ -11,3 +11,21 @@ const port = process.env.PORT || 4005
 app.listen(port, () => {
   console.log(`Shai Halud Seen on ${port}`)
 })
+
+// Other ways to link files for deployment
+// JavaScript:
+
+// app.get('/js', (req, res) => {
+//   res.sendFile(path.join(__dirname, './public/main.js'))
+// })
+// HTML:
+
+// <script src="/js"></script>
+// Use middleware to serve the files on each request.
+
+// JavaScript:
+
+// app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
+// HTML:
+
+// <script src="/js"></script>
